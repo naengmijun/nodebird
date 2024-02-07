71f16b8cfd0c7312acd6eeb7305ef508dd9a28cb
이준호 <junho@ijunhoui-MacBookAir.local>
2024-02-07 12:14:57 +0900

    프론트 화면 html 구성

--- routes/page.js
+++ routes/page.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { renderJoin, renderMain, renderProfile } = require('../controllers/page');
 
+router.use((req, res, next) => {
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
+});
+
 router.get('/profile', renderProfile);
 router.get('/join', renderJoin);
 router.get('/', renderMain);
